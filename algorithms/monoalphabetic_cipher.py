8e5bf0a8bea18bc9817bd595ad19fa65e4972ecb
Anton Kriese <anton.kriese@gmx.de>
2021-11-01 15:43:49 +0100

    basic permutation cipher implementation

---
+++ algorithms/monoalphabetic_cipher.py
@@ -0,0 +1,20 @@
+from shift_cipher import str2ord, ord2str
+import random
+
+class PermutationCipher():
+    def __init__(self):
+        self.generate(random.random())
+
+    def generate(self, key):
+        alphabet = [chr(x) for x in range(ord('a'), ord('z')+1)]
+        shuffle_alphabet = alphabet.copy()
+        random.seed(key)
+        random.shuffle(shuffle_alphabet)
+        self.enc_dict = {c: e for c, e in zip(alphabet, shuffle_alphabet)}
+        self.dec_dict = {e: c for c, e in zip(alphabet, shuffle_alphabet)}
+
+    def encode(self, message):
+        return [self.enc_dict[c] for c in message]
+
+    def decode(self, message):
+        return "".join([self.dec_dict[e] for e in message])
